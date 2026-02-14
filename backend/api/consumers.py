7c3ba55f472f75f63aebda4fe6e63e5d7e37b760
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2026-02-14 08:54:45 -0400

    Cria consumer websocket

---
+++ backend/api/consumers.py
@@ -0,0 +1,18 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class MonitorConsumer(AsyncWebsocketConsumer):
+    
+    async def connect(self):
+        self.grou_name = 'monitor'
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.grou_name, self.channel_name)
+
+    async def broadcast_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message']
+        }))
