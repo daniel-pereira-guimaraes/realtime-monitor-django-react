51b391931d4f331bfd10c7934a57479ce697b9f8
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2026-02-14 10:22:12 -0400

    Corrige nome de variável no MonitorConsumer do backend

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -10,7 +10,7 @@ class MonitorConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(self.grou_name, self.channel_name)
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def broadcast_message(self, event):
         await self.send(text_data=json.dumps({
