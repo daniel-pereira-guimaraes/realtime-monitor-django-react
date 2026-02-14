f142a0948b50d0850f0e27a7a67e352f5632396d
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2026-02-14 09:04:19 -0400

    Cria endpoint POST para envio de mensagem

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
-# Create your views here.
+
+@csrf_exempt
+def send_message(request):
+    
+    if request.method == 'POST':
+    
+        data = json.loads(request.body)
+        message = data.get('message', '')
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'monitor',
+            {
+                'type': 'broadcast_message',
+                'message': message
+            }
+        )
+
+        return JsonResponse({'status': 'ok'})
